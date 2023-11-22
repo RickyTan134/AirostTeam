3b0eae14e61993dfe0c51b45e59a38a2ae5fddc3
RickyTan134 <148662535+RickyTan134@users.noreply.github.com>
2023-11-22 19:59:42 +0800

    Add files via upload

---
+++ signin.js
@@ -0,0 +1,2 @@
+document.querySelector("#show-login").addEventListener("click",function(){document.querySelector(".popup").classList.add("active");});
+document.querySelector(".popup .close-btn").addEventListener("click",function(){document.querySelector(".popup").classList.remove("active");});
